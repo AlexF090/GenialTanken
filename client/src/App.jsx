8433784be43660654203862f84950a470cc05c45
Dennis Koch <denniskoch@Denniss-Air.fritz.box>
2022-04-27 16:13:39 +0200

    add styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,66 @@
 import React from 'react';
-import Header from './components/Header'
-import NavBar from './components/NavBar'
+import styled from 'styled-components';
+import Header from './components/Header';
+import NavBar from './components/NavBar';
 import './App.css';
 import data from './data/db';
-import FavoriteIconDeactivated from './img/favorite_icon_deactivated.svg'
-
+import FavoriteIconDeactivated from './icons/favorite_icon_deactivated.svg';
 
 function App() {
   console.log(data);
 
   return (
-    <div className="App">
+    <Wrapper>
       <Header />
-      <ul className="Ulist">
+      <Main>
+      <StyledUList>
         {data.map(station => (
-          <li className="ListItems" key={station.uuid}>
-            {station.e5} {station.name.length > 14 ?
-  `${station.name.substring(0, 14)}...` : station.name} {station.street} {station.post_code} {station.city}
+          <StyledGasStationItem className="ListItems" key={station.uuid}>
+            {station.e5}{' '}
+            {station.name.length > 14 ? `${station.name.substring(0, 14)}...` : station.name}{' '}
+            {station.street} {station.post_code} {station.city}
             <img src={FavoriteIconDeactivated} alt="Favorite icon deactivated" />
-          </li>
+          </StyledGasStationItem>
         ))}
-      </ul>
+      </StyledUList>
+      </Main>
       <NavBar />
-    </div>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  
+`;
+
+const Main = styled.main`
+  display: flex;
+  width: 70vw;y<
+  justify-content: center;
+  
+`;
+
+const StyledUList = styled.ul`
+display: flex;
+margin-top: 120px;
+margin-bottom: 100px;
+flex-direction: column;
+gap: 10px;
+`;
+
+const StyledGasStationItem = styled.li`
+  box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25), inset 0px 0px 2px rgba(0, 0, 0, 0.25);
+  border-radius: 10px;
+  margin-bottom: 4px;
+  padding: 15px;
+  list-style: none;
+  /* border: 1px solid black; */
+`;
+
+//
+//
 
 export default App;
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import styled from 'styled-components';
 
 function Header() {
   return (
-    <h1>Header</h1>
+    <StyledHeader>Header</StyledHeader>
   )
 }
+
+const StyledHeader = styled.h1`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 6.25rem;;
+  position: fixed;
+  top: 0;
+  border: 1px solid black;
+  
+  background-color: #ffffff;
+`;
 
 export default Header;
