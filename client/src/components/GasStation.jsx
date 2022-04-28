8a14b85966a274512a0edea892b0267753a6c949
Dennis Koch <denniskoch@Denniss-Air.fritz.box>
2022-04-28 19:18:03 +0200

    list styling gridbox

--- client/src/components/GasStation.jsx
+++ client/src/components/GasStation.jsx
@@ -5,21 +5,23 @@ import FavoriteIconDeactivated from '../icons/favorite_icon_deactivated.svg';
 
 function GasStation() {
   return (
-    
     <GasStationList role="list">
       {data.map(station => (
         <GasStationItem className="ListItems" key={station.uuid}>
           <Price>{station.e5}</Price>
           <Brand>{station.brand}</Brand>
-          <Name>{station.name.length > 14 ? `${station.name.substring(0, 14)}...` : station.name}</Name>
+          <Name>
+            {station.name.length > 14 ? `${station.name.substring(0, 14)}...` : station.name}
+          </Name>
           <Street>{station.street}</Street>
-          <Adress>{station.post_code} {station.city}</Adress>
-          
+          <Adress>
+            {station.post_code} {station.city}
+          </Adress>
+
           <FavoriteIcon src={FavoriteIconDeactivated} alt="Favorite icon deactivated" />
         </GasStationItem>
       ))}
     </GasStationList>
-    
   );
 }
 
@@ -33,55 +35,50 @@ const GasStationList = styled.ul`
 `;
 
 const GasStationItem = styled.li`
-    display: grid; 
-    width: 100%;
-  grid-template-columns: 0.1fr 1fr 0.1fr 1fr 1fr 0.1fr; 
-  grid-template-rows: 1fr 1fr 1fr; 
-  gap: 0px 0px; 
-  grid-template-areas: 
-  ". gasStationName . brand . ."
-    "price street . . . favoriteIcon"
-    ". adress city . . .";
+  display: grid;
+  width: 100%;
+  grid-template-columns: 0.1fr 1fr 0.1fr 1fr 1fr 0.1fr;
+  grid-template-rows: 1fr 1fr 1fr;
+  gap: 0px 0px;
+  grid-template-areas:
+    '. gasStationName . brand . .'
+    'price street . . . favoriteIcon'
+    '. adress city . . .';
   box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25), inset 0px 0px 2px rgba(0, 0, 0, 0.25);
   border-radius: 10px;
   margin-bottom: 4px;
   padding: 15px;
   list-style: none;
-  
 `;
 
-
-
 const Price = styled.p`
-grid-area: price;
-justify-self: start;
-margin-right: 1em;
-font-size: 1.5rem;
+  grid-area: price;
+  justify-self: start;
+  margin-right: 1em;
+  font-size: 1.5rem;
 `;
 
 const Brand = styled.p`
-grid-area: brand;
-
+  grid-area: brand;
 `;
 
 const Name = styled.p`
-grid-area: gasStationName;
+  grid-area: gasStationName;
 `;
 
 const Street = styled.p`
-grid-area: street;
-font-size: 0.75rem;
+  grid-area: street;
+  font-size: 0.75rem;
 `;
 
 const Adress = styled.p`
-grid-area: adress;
-font-size: 0.75rem;
+  grid-area: adress;
+  font-size: 0.75rem;
 `;
 
 const FavoriteIcon = styled.img`
-grid-area: favoriteIcon;
-justify-self: end;
+  grid-area: favoriteIcon;
+  justify-self: end;
 `;
-
 
 export default GasStation;
