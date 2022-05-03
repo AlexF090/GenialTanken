9169d3d40ec6a38948301b495c6a264eb04351fd
Alex Fischenich <alexander.fischenich@pm.me>
2022-05-03 18:29:35 +0200

    update OpeningTimes.jsx

--- client/src/components/OpeningTimes.jsx
+++ client/src/components/OpeningTimes.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 
 function OpeningTimes({ currentStation }) {
+  const openingtimesJson = currentStation.openingtimes_json;
+  // const openingTimes = currentStation.openingtimes_json.openingTimes;
+
   const convertOpeningTimes = () => {
     // console.log(`Test: ${Object.keys(currentStation.openingtimes_json.openingTimes[0])}`);
-    if (Object.keys(currentStation.openingtimes_json).length === 0) {
+    if (Object.keys(openingtimesJson).length === 0) {
       return (
         <p>Uns liegen leider keine Informationen zu den Öffnungzeiten dieser Tankstelle vor</p>
       );
-    } else return <p>Hier liegen Öffnungszeit der Tankstelle vor.</p>;
+    } else return <p>Hier liegen Öffnungszeiten der Tankstelle vor.</p>;
   };
 
-  return <div>{convertOpeningTimes()}</div>;
+  return <p>{convertOpeningTimes()}</p>;
 }
 
 export default OpeningTimes;
