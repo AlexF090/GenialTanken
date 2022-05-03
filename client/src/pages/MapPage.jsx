366537796b9f8b21f739d0aae021db67ccf61ec4
Alex Fischenich <alexander.fischenich@pm.me>
2022-05-03 13:39:58 +0200

    remove the wrong component

--- client/src/pages/MapPage.jsx
+++ client/src/pages/MapPage.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import Header from '../components/Header.jsx';
-import GasStationDetail from '../components/GasStationDetail.jsx';
 
 function MapPage({ title }) {
   return (
     <>
       <Header title={title} />
-      <GasStationDetail />
     </>
   );
 }
