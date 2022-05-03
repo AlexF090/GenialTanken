43a2baac15687a6f7b63889fbd7dfc0ee55e7364
Alex Fischenich <alexander.fischenich@pm.me>
2022-05-03 16:14:26 +0200

    create OpeningTimes.jsx

---
+++ client/src/components/OpeningTimes.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function OpeningTimes({ currentStation }) {
+  const convertOpeningTimes = () => {
+    // console.log(`Test: ${Object.keys(currentStation.openingtimes_json.openingTimes[0])}`);
+    if (Object.keys(currentStation.openingtimes_json).length === 0) {
+      return (
+        <p>Uns liegen leider keine Informationen zu den Öffnungzeiten dieser Tankstelle vor</p>
+      );
+    } else return <p>Hier liegen Tankstellenpreise vor.</p>;
+  };
+
+  return <div>{convertOpeningTimes()}</div>;
+}
+
+export default OpeningTimes;
