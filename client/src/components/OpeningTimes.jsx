397db46d2fb116f6e59827a25fade1e66155291c
Dennis Koch <denniskoch249@gmx.de>
2022-05-04 10:43:59 +0200

    clean coding
    Co-authored-by: AlexF090 <AlexF090@users.noreply.github.com>

--- client/src/components/OpeningTimes.jsx
+++ client/src/components/OpeningTimes.jsx
@@ -2,14 +2,10 @@ import React from 'react';
 
 function OpeningTimes({ currentStation }) {
   const openingtimesJson = currentStation.openingtimes_json;
-  // const openingTimes = currentStation.openingtimes_json.openingTimes;
 
   const convertOpeningTimes = () => {
-    // console.log(`Test: ${Object.keys(currentStation.openingtimes_json.openingTimes[0])}`);
     if (Object.keys(openingtimesJson).length === 0) {
-      return (
-        <>Uns liegen leider keine Informationen zu den Öffnungzeiten dieser Tankstelle vor</>
-      );
+      return <>Uns liegen leider keine Informationen zu den Öffnungzeiten dieser Tankstelle vor</>;
     } else return <>Hier liegen Öffnungszeiten der Tankstelle vor.</>;
   };
 
