e382f97d9813a420cb53725ba7e2260aaf15ff33
Dennis Koch <denniskoch249@gmx.de>
2022-05-06 17:54:26 +0200

    add .env
    Co-authored-by: AlexF090 <AlexF090@users.noreply.github.com>

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,19 +1,12 @@
 import React, { useState, useRef, useEffect } from 'react';
-
+import styled from 'styled-components';
+import { IoIosPin } from 'react-icons/io';
 import mapboxgl from 'mapbox-gl';
-mapboxgl.accessToken =
-  'pk.eyJ1IjoiYWxleGYwOTAiLCJhIjoiY2wydWhlNzlwMDIzbDNkbXBjM2s4YXh5cSJ9.vMlWcn3nD2YpsFwSbhg7sw';
-
-const style = {
-  position: 'absolute',
-  top: 0,
-  right: 0,
-  left: 0,
-  bottom: 0,
-};
+const mapToken = process.env.REACT_APP_API_KEY;
+mapboxgl.accessToken = mapToken;
 
 function Map() {
-  const [state, setState] = useState({
+  const [coords, setCoords] = useState({
     lng: 13.376696,
     lat: 52.500478,
     zoom: 18,
@@ -23,19 +16,30 @@ function Map() {
   const map = useRef(null);
 
   useEffect(() => {
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: 'mapbox://styles/alexf090/cl2uhl8gx008p14qop192uczn',
-      center: [state.lng, state.lat],
-      zoom: state.zoom,
-    });
+    if (map.current === null) {
+      map.current = new mapboxgl.Map({
+        container: mapContainer.current,
+        style: 'mapbox://styles/alexf090/cl2uhl8gx008p14qop192uczn',
+        center: [coords.lng, coords.lat],
+        zoom: coords.zoom,
+      });
+    }
   }, []);
 
   return (
-    <div className="App">
-      <div style={style} ref={el => (mapContainer.current = el)} />
-    </div>
+    <>
+      <MapStyle ref={el => (mapContainer.current = el)} />
+    </>
   );
 }
+
+const MapStyle = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 0;
+  overflow: hidden;
+`;
 
 export default Map;
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import GlobalStyle from './GlobalStyles';
+import App from './App.jsx';
+import GlobalStyle from './GlobalStyles.jsx';
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
