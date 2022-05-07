10654970812713a8dc513e8ed9d046664bd95f47
Dennis Koch <denniskoch249@gmx.de>
2022-05-07 14:23:40 +0200

    NavBar iOs fix & crash fix openingTimes
    Co-authored-by: AlexF090 <AlexF090@users.noreply.github.com>

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -36,7 +36,7 @@ const Navigation = styled.nav`
   display: flex;
   flex-wrap: wrap;
   width: 100%;
-  height: 4.6875rem;
+  height: 6em;
   position: fixed;
   bottom: 0;
   border-top: 1px solid black;
