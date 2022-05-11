b7a6b8ad11405f44dee49cee9dfdabb52e6b7f60
Dennis Koch <denniskoch249@gmx.de>
2022-05-11 13:56:25 +0200

    fix vercel bug Co-authored-by: AlexF090 <AlexF090@users.noreply.github.com>

--- client/src/components/map/LocationMarker.jsx
+++ client/src/components/map/LocationMarker.jsx
@@ -4,16 +4,17 @@ import { useMapEvents, Marker, Popup } from 'react-leaflet';
 function LocationMarker() {
   const [position, setPosition] = useState(null);
 
-  useEffect(() => {
-    map.locate();
-  }, []);
-
   const map = useMapEvents({
     locationfound(e) {
       setPosition(e.latlng);
       map.flyTo(e.latlng, map.getZoom());
     },
   });
+
+  useEffect(() => {
+    map.locate();
+  }, []);
+
   return position === null ? null : (
     <Marker position={position}>
       <Popup> Du bist hier !</Popup>
