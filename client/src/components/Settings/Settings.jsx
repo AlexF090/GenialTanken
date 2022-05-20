d8fad3116baa77d830feaef95f5b739ae908e31f
Dennis Koch <denniskoch249@gmx.de>
2022-05-20 14:35:49 +0200

    fix error in settings.jsx
    Co-authored-by: AlexF090
    <AlexF090@users.noreply.github.com>

--- client/src/components/Settings/Settings.jsx
+++ client/src/components/Settings/Settings.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import Button from './Button/Button';
+import Button from '../Button/Button.jsx';
 
 function Settings({ fuelValue, setFuelValue, getCurrentPosition, radius, setRadius }) {
   return (
