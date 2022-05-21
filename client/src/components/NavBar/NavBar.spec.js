cdd955ab43b3c6b0e48d8bcbefe0f359655b4d6a
Dennis Koch <denniskoch249@gmx.de>
2022-05-21 13:24:42 +0200

    edit expect(links.length) to 2
    Co-authored-by: AlexF090
    <AlexF090@users.noreply.github.com>

--- client/src/components/NavBar/NavBar.spec.js
+++ client/src/components/NavBar/NavBar.spec.js
@@ -1,6 +1,6 @@
-import { render, screen } from '@testing-library/react'
-import { BrowserRouter as Router } from 'react-router-dom'
-import NavBar from './NavBar'
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import NavBar from './NavBar';
 
 describe('Navigation', () => {
   it('renders the navigation', () => {
@@ -8,19 +8,19 @@ describe('Navigation', () => {
       <Router>
         <NavBar />
       </Router>
-    )
+    );
 
-    const navigation = screen.getByRole('navigation')
-    expect(navigation).toBeInTheDocument()
-  })
+    const navigation = screen.getByRole('navigation');
+    expect(navigation).toBeInTheDocument();
+  });
 
   it('has at leat one nav link', () => {
     render(
       <Router>
         <NavBar />
       </Router>
-    )
-    const links = screen.getAllByRole('link')
-    expect(links.length).toBeGreaterThan(0)
-  })
-})
+    );
+    const links = screen.getAllByRole('link');
+    expect(links.length).toBeGreaterThan(2);
+  });
+});
